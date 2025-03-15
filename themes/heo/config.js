4c01f7e8dfd81bbd0650b8391fb937e80391e017
Aerosand <aerosand@outlook.com>
2025-03-15 16:18:20 +0900

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -52,8 +52,7 @@ const CONFIG = {
   HEO_GROUP_ICONS: [
     {
       title_1: 'AfterEffect',
-      // img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      img_1: 'https://images.unsplash.com/photo-1736842666465-5234629bff2b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
       title_2: 'Sketch',
       img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
